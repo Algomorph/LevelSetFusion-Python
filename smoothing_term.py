Another smoothing term test -- revealed (potential) bug in vectorized energy computation

smoothing_term.py
@@ -21,7 +21,7 @@ import scipy
 
 from utils.sampling import focus_coordinates_match, sample_warp_replace_if_zero, sample_warp
 from utils.tsdf_set_routines import set_zeros_for_values_outside_narrow_band_union_multitarget, \
-    value_outside_narrow_band, voxel_is_outside_narrow_band_union
+    value_outside_narrow_band, voxel_is_outside_narrow_band_union, set_zeros_for_values_outside_narrow_band_union
 
 
 class SmoothingTermMethod(Enum):
@@ -168,10 +168,15 @@ def compute_smoothing_term_energy(warp_field, warped_live_field=None, canonical_
     warp_gradient_u_x, warp_gradient_u_y = np.gradient(warp_field[:, :, 0])
     warp_gradient_v_x, warp_gradient_v_y = np.gradient(warp_field[:, :, 1])
 
+    # if band_union_only:
+    #     set_zeros_for_values_outside_narrow_band_union_multitarget(warped_live_field, canonical_field,
+    #                                                                (warp_gradient_u_x, warp_gradient_u_y,
+    #                                                                 warp_gradient_v_x, warp_gradient_v_y))
+
+    gradient_aggregate = \
+        warp_gradient_u_x ** 2 + warp_gradient_v_x ** 2 + warp_gradient_u_y ** 2 + warp_gradient_v_y ** 2
     if band_union_only:
-        set_zeros_for_values_outside_narrow_band_union_multitarget(warped_live_field, canonical_field,
-                                                                   (warp_gradient_u_x, warp_gradient_u_y,
-                                                                    warp_gradient_v_x, warp_gradient_v_y))
+        set_zeros_for_values_outside_narrow_band_union(warped_live_field, canonical_field, gradient_aggregate)
 
     smoothing_energy = 0.5 * np.sum(
         warp_gradient_u_x ** 2 + warp_gradient_v_x ** 2 + warp_gradient_u_y ** 2 + warp_gradient_v_y ** 2)

tests/test_smoothing_term.py
@@ -27,20 +27,84 @@ class SmoothingTermTest(TestCase):
 
         live_field = np.array([[0.3, 0.5],
                                [-0.8, 1.]])
-        canoincal_field = np.array([[0.5, -0.3],
+        canonical_field = np.array([[0.5, -0.3],
                                     [1., 0.]])
 
         expected_gradient_out = np.array([[[-2., -2.], [1., 1.]],
                                           [[1., 1.], [0., 0.]]], dtype=np.float32)
         expected_energy_out = 1.0
 
-        gradient_out, energy_out = st.compute_smoothing_term_gradient_direct(warp_field, live_field, canoincal_field)
+        gradient_out, energy_out = st.compute_smoothing_term_gradient_direct(warp_field, live_field, canonical_field)
 
         self.assertTrue(np.allclose(gradient_out, expected_gradient_out))
-        self.assertTrue(energy_out, expected_gradient_out)
+        self.assertEqual(energy_out, expected_energy_out)
 
         gradient_out = st.compute_smoothing_term_gradient_vectorized(warp_field)
-        energy_out = st.compute_smoothing_term_energy(warp_field, live_field, canoincal_field)
+        energy_out = st.compute_smoothing_term_energy(warp_field, live_field, canonical_field)
 
         self.assertTrue(np.allclose(gradient_out, expected_gradient_out))
-        self.assertTrue(energy_out, expected_gradient_out)
+        self.assertEqual(energy_out, expected_energy_out)
+
+    def test_smoothing_term02(self):
+        warped_live_field = np.array([[1., 1., 0.49999955, 0.42499956],
+                                      [1., 0.44999936, 0.34999937, 0.32499936],
+                                      [1., 0.35000065, 0.25000066, 0.22500065],
+                                      [1., 0.20000044, 0.15000044, 0.07500044]], dtype=np.float32)
+        canonical_field = np.array([[1.0000000e+00, 1.0000000e+00, 3.7499955e-01, 2.4999955e-01],
+                                    [1.0000000e+00, 3.2499936e-01, 1.9999936e-01, 1.4999935e-01],
+                                    [1.0000000e+00, 1.7500064e-01, 1.0000064e-01, 5.0000645e-02],
+                                    [1.0000000e+00, 7.5000443e-02, 4.4107438e-07, -9.9999562e-02]], dtype=np.float32)
+
+        warp_field = np.array([[[0., 0.],
+                                [0., 0.],
+                                [-0.35937524, -0.18750024],
+                                [-0.13125, -0.17500037], ],
+
+                               [[0., 0.],
+                                [-0.4062504, -0.4062496],
+                                [-0.09375, -0.1874992],
+                                [-0.04375001, -0.17499907], ],
+
+                               [[0., 0.],
+                                [-0.65624946, -0.21874908],
+                                [-0.09375, -0.1499992],
+                                [-0.04375001, -0.21874908], ],
+
+                               [[0., 0.],
+                                [-0.5312497, -0.18750025],
+                                [-0.09374999, -0.15000032],
+                                [-0.13125001, -0.2625004], ]], dtype=np.float32)
+
+        expected_gradient_out = np.array([[[0., 0.],
+                                           [0., 0.],
+                                           [-0.8531257, -0.20000115],
+                                           [0.14062527, 0.01249856]],
+
+                                          [[0., 0.],
+                                           [-0.8750021, -1.2187502],
+                                           [0.52812564, 0.16875133],
+                                           [0.13749997, 0.05625144]],
+
+                                          [[0., 0.],
+                                           [-1.5937477, -0.13124725],
+                                           [0.51249945, 0.17500085],
+                                           [0.13749999, -0.06874855]],
+
+                                          [[0., 0.],
+                                           [-0.8437496, -0.19375136],
+                                           [0.47499973, 0.14999892],
+                                           [-0.125, -0.1562514]]], dtype=np.float32)
+        expected_energy_out = 0.2126010925276205
+
+        smoothing_gradient_out, energy_out = \
+            st.compute_smoothing_term_gradient_direct(warp_field, warped_live_field, canonical_field)
+
+        self.assertTrue(np.allclose(smoothing_gradient_out, expected_gradient_out))
+        self.assertEqual(energy_out, expected_energy_out)
+
+        smoothing_gradient_out = st.compute_smoothing_term_gradient_vectorized(warp_field)
+        set_zeros_for_values_outside_narrow_band_union(warped_live_field, canonical_field, smoothing_gradient_out)
+        energy_out = st.compute_smoothing_term_energy(warp_field, warped_live_field, canonical_field)
+
+        self.assertTrue(np.allclose(smoothing_gradient_out, expected_gradient_out))
+        self.assertEqual(energy_out, expected_energy_out)